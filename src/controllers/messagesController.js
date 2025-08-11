422678964a89a760067a2fdab0c685cbb24d340c
Kobi Hanoch <kobikobi622@gmail.com>
2025-08-11 11:09:02 +0300

    feat(Messages): Created endpoints for mark as read and delete messages

--- src/controllers/messagesController.js
+++ src/controllers/messagesController.js
@@ -12,3 +12,38 @@ export const getAllUserMessages = async (req, res) => {
 
   return res.status(200).json(messages);
 };
+
+// -----------------------------------
+// 1. Need to check both below endpoints
+// 2. Need to create a method for sending a system message
+// 3. Check 2
+
+// @desc    Update message to read
+// @route   PUT /api/messages/markmasread/:id
+// @access  Private
+export const markUserMessageAsRead = async (req, res) => {
+  // Update the message (won't e effective if trying to change other user's message)
+  const rows =
+    await sql`UPDATE messages AS m SET is_read = TRUE WHERE m.id=${req.params.id} AND m.receiver_id=${req.user.id}
+     RETURNING id, is_read`;
+
+  // If message don't exist
+  if (!rows.length) {
+    throw createError(404, "Message not found");
+  }
+
+  // Success
+  return res.status(200).json(rows[0]);
+};
+
+// @desc    Delete message
+// @route   DELETE /api/messages/delete/:id
+// @access  Private
+export const deleteMessage = async (req, res) => {
+  const rows =
+    await sql`DELETE FROM messages AS m WHERE m.id=${req.params.id} AND (m.receiver_id=${req.user.id} OR m.sender_id=${req.user.id}) RETURNING id`;
+  if (!rows.length) {
+    throw createError(404, "Message not found.");
+  }
+  return res.status(200).json(rows[0]);
+};
--- src/routes/messagesRoutes.js
+++ src/routes/messagesRoutes.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
 import {
-  getExerciseTracking,
-  getWholeUserWorkoutPlan,
-} from "../controllers/workoutController.js";
+  deleteMessage,
+  getAllUserMessages,
+  markUserMessageAsRead,
+} from "../controllers/messagesController.js";
 import { asyncHandler } from "../middlewares/asyncHandler.js";
 import { protect } from "../middlewares/authMiddleware.js";
-import { getAllUserMessages } from "../controllers/messagesController.js";
 
 const router = Router();
 
 // User Routes
 router.get("/getmessages", protect, asyncHandler(getAllUserMessages)); // Gets user messages
+router.put("/markasread/:id", protect, asyncHandler(markUserMessageAsRead)); // Gets user messages
+router.delete("/delete/:id", protect, asyncHandler(deleteMessage)); // Deletes a user's message
 
 export default router;
