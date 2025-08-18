5130889a7fa51136684d56f3e97e5144156cc5ca
Kobi Hanoch <kobikobi622@gmail.com>
2025-08-18 11:10:04 +0300

    docs(WorkoutRoutes): Comment change

--- src/routes/workoutRoutes.js
+++ src/routes/workoutRoutes.js
@@ -15,7 +15,7 @@ const router = Router();
 router.get("/getworkout", protect, asyncHandler(getWholeUserWorkoutPlan)); // Gets workout plan (whole)
 router.get("/gettracking", protect, asyncHandler(getExerciseTracking)); // Gets exercise tracking
 router.post("/finishworkout", protect, asyncHandler(finishUserWorkout)); // Save user's finished workout
-router.delete("/delete", protect, asyncHandler(deleteUserWorkout)); // Save user's finished workout
+router.delete("/delete", protect, asyncHandler(deleteUserWorkout)); // Deletes user's workout
 router.post("/add", protect, asyncHandler(addWorkout)); // Add new workout
 
 export default router;
