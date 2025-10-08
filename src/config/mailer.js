3c0b9245e63170277ce87cbbebfc90e96787ffa7
Kobi Hanoch <kobikobi622@gmail.com>
2025-10-08 18:36:58 +0300

    Email config

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -6,10 +6,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendMail = async ({ to, subject, html }) => {
   try {
     await resend.emails.send({
-      from: "Strong Together <no-reply@auth.kobihanoch.com>",
+      from: "Strong Together <support@auth.kobihanoch.com>",
       to,
       subject,
       html,
+      replyTo: "support@auth.kobihanoch.com",
     });
     console.log("An email has been sent to", to);
   } catch (err) {
