fd5c87746aa2c6df61d89deb28dcbfb17df292bb
Kobi Hanoch <kobikobi622@gmail.com>
2026-03-28 18:37:45 +0300

    Fixed OAuth Apple request body

--- src/validators/oAuth/appleOAuthRequest.schema.ts
+++ src/validators/oAuth/appleOAuthRequest.schema.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod';
 
 const appleNameInput = z.object({
-  givenName: z.string().optional(),
-  familyName: z.string().optional(),
+  givenName: z.string().nullable(),
+  familyName: z.string().nullable(),
 });
 
 export const appleOAuthRequest = z.object({
   body: z.object({
-    idToken: z.string().optional(),
-    rawNonce: z.string().optional(),
-    name: appleNameInput.optional().nullable(),
-    email: z.string().email(),
+    idToken: z.string(),
+    rawNonce: z.string(),
+    name: appleNameInput.optional(),
+    email: z.string().email().nullable(),
   }),
 });
