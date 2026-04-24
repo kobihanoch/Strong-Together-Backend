aebd0e15e623ba762eddaa3775b96ae5649b6734
Kobi Hanoch <kobikobi622@gmail.com>
2026-04-24 15:48:06 +0300

    Added comments to AWS init script for better readability

--- scripts/localstack/init-aws.sh
+++ scripts/localstack/init-aws.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 set -e
 
-BUCKET_NAME="${AWS_BUCKET_NAME:-strong-together-videos-dev}"
-PROFILE_BUCKET_NAME="${BUCKET_NAME_PROFILE:-profile-pics}"
-QUEUE_NAME="${AWS_ANALYSIS_QUEUE_NAME:-s3-upload-processing-queue}"
+# Global vars
+VIDEO_PROCESSING_BUCKET_NAME="${AWS_BUCKET_NAME}"
+PROFILE_BUCKET_NAME="profile-pics"
+QUEUE_NAME="s3-upload-processing-queue"
 
-awslocal s3api create-bucket --bucket "$BUCKET_NAME" >/dev/null 2>&1 || true
+# Create buckets and SQS queue
+# 2>&1 => 1 is output, 2 is error => Redirect the output of errors as output
+awslocal s3api create-bucket --bucket "$VIDEO_PROCESSING_BUCKET_NAME" >/dev/null 2>&1 || true
 awslocal s3api create-bucket --bucket "$PROFILE_BUCKET_NAME" >/dev/null 2>&1 || true
 awslocal sqs create-queue --queue-name "$QUEUE_NAME" >/dev/null
 
+# SQS queue URL and ARN (for S3 event notifications)
 QUEUE_URL="$(awslocal sqs get-queue-url --queue-name "$QUEUE_NAME" --output text --query QueueUrl)"
 QUEUE_ARN="$(awslocal sqs get-queue-attributes --queue-url "$QUEUE_URL" --attribute-names QueueArn --output text --query Attributes.QueueArn)"
 
+# Create S3 event notification policy
 cat <<EOF >/tmp/s3-event-notification.json
 {
   "QueueConfigurations": [
@@ -23,6 +28,7 @@ cat <<EOF >/tmp/s3-event-notification.json
 }
 EOF
 
+# Attach S3 event notification policy to video analysis S3 bucket
 awslocal s3api put-bucket-notification-configuration \
-  --bucket "$BUCKET_NAME" \
+  --bucket "$VIDEO_PROCESSING_BUCKET_NAME" \
   --notification-configuration file:///tmp/s3-event-notification.json
