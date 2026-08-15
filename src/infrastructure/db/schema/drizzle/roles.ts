c5eead62071132bfffa858d06affa6a452b6bb79
Kobi Hanoch <kobikobi622@gmail.com>
2026-08-16 01:17:09 +0300

    Add guest role and app runtime user role to the role schema

--- src/infrastructure/db/schema/drizzle/roles.ts
+++ src/infrastructure/db/schema/drizzle/roles.ts
@@ -2,5 +2,11 @@ import { pgRole } from 'drizzle-orm/pg-core';
 
 export const anonRole = pgRole('anon');
 export const authenticatedRole = pgRole('authenticated');
+export const guestRole = pgRole('guest');
 export const serviceRole = pgRole('service_role');
 export const appUserRole = pgRole('app_user');
+export const appRuntimeUserRole = pgRole('app_runtime_user', {
+  createDb: false,
+  createRole: false,
+  inherit: false,
+});
